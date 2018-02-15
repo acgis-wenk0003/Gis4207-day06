fb834877c28598ff0d34dd84904a53fd5ae5bf6d
LAPTOP-KV4DTCCF\chris <wenk0003@algonquinlive.com>
2018-02-15 11:24:13 -0500

    working LoadLayerVisibility

---
+++ ChrisWandMorganH/LoadLayerVisibility.py
@@ -0,0 +1,23 @@
+import arcpy
+
+mxd=arcpy.mapping.MapDocument("CURRENT")
+infile=open("LayerVisibility.txt")
+txtlayers=[]
+maplayers=[]
+for line in infile:
+        split=line.strip('\n').split(',')
+        txtlayers.append(split)
+for df in arcpy.mapping.ListDataFrames(mxd):
+    for layer in arcpy.mapping.ListLayers(mxd,'',df):
+        for txtlayer in txtlayers:
+            if layer.name==txtlayer[1]:
+                if layer.visible!=txtlayer[2]:
+                    layer.visible=txtlayer[2]
+                else:
+                    continue
+            else:
+                continue
+infile.close()
+del mxd
+arcpy.RefreshActiveView()
+arcpy.RefreshTOC()
